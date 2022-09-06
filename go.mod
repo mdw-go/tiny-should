7774492745688e42808b494b2cadd70f9506d69a
Michael Whatcott <mdwhatcott@users.noreply.github.com>
2022-09-05 23:11:59 -0600

    A tiny version of github.com/mdwhatcott/testing/should.

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/mdwhatcott/tiny-should
+
+go 1.19
---
+++ some_test.go
@@ -0,0 +1,24 @@
+package tiny_should
+
+import (
+	"testing"
+
+	"github.com/mdwhatcott/tiny-should/should"
+)
+
+func TestPassing(t *testing.T) {
+	should.So(t, 1, should.Equal, 1)
+	should.So(t, false, should.BeFalse)
+	should.So(t, true, should.BeTrue)
+	should.So(t, nil, should.BeNil)
+	should.So(t, 1, should.NOT.BeNil)
+}
+func TestFailing(t *testing.T) {
+	t.Skip("comment me to see the failures below")
+	should.So(nil, 1, should.Equal, 2)
+	should.So(t, 1, should.Equal, 2)
+	should.So(t, true, should.BeFalse)
+	should.So(t, false, should.BeTrue)
+	should.So(t, 1, should.BeNil)
+	should.So(t, nil, should.NOT.BeNil)
+}
